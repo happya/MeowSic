efffe70c4c250681717f5946803de059601b56f0
happya <whuyaya@gmail.com>
2019-07-14 01:29:48 +0800

    finish the basic frame of singer-detail page

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -10,7 +10,7 @@
     <!-- 歌手名字 -->
     <h1 class="title" v-html="title"></h1>
     <!-- 歌手图片 -->
-    <div class="bg-image"></div>
+    <div class="bg-image" :style="bgStyle" ref="bgImage"></div>
     <!-- 滚动时的遮挡层-->
     <div class="bg-layer"></div>
     <!-- 歌曲列表组件 -->
@@ -40,6 +40,8 @@
 import BaseScroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
 import BaseLoading from 'base/loading/loading'
+
+// const RESERVED_HEIGHT = 40
 export default {
   name: 'MusicList',
   props: {
@@ -57,6 +59,15 @@ export default {
       type: String,
       default: ''
     }
+  },
+  computed: {
+    bgStyle() {
+      return `background-image: url(${this.bgImage})`
+    }
+  },
+  mounted() {
+    this.imageHeight = this.$refs.bgImage.clientHeight
+    this.$refs.list.$el.style.top = `${this.imageHeight}px`
   },
   created() {
     this.probeType = 3
@@ -90,9 +101,35 @@ export default {
       position: absolute
       top: 0
       left: 6px
+      z-index: 50
       .icon-back
         display: block
         padding: 10px
         font-size: $font-size-large-x
         color: $color-theme
+    .title
+      position: absolute
+      top: 0
+      left: 10%
+      width: 80%
+      z-index: 40
+      no-wrap()
+      text-align: center
+      line-height: 40px
+      font-size: $font-size-large
+      color: $color-text
+    .bg-image
+      position: relative
+      width: 100%
+      height: 0
+      padding-top: 70%
+      background-size: cover
+    .list
+      position: absolute
+      top: 0
+      bottom: 0
+      width: 100%
+      background: $color-background
+      .song-list-wrapper
+        padding: 20px 30px
 </style>
