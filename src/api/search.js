aa6ef65a768250c5fa9f787f4adafa8e1cd9775f
happya <whuyaya@gmail.com>
2019-07-22 21:24:51 +0800

    change api

--- src/api/search.js
+++ src/api/search.js
@@ -16,7 +16,8 @@ export function getHotkey() {
 
 export function search(query, page, zhida, perpage) {
   // const url = '/api/search'
-  const url = debug ? '/api/search' : '101.132.193.166/music/api/search'
+  // const url = debug ? '/api/search' : '101.132.193.166/music/api/search'
+  const url = debug ? '/api/search' : 'http://www.lionust.tech/music/api/search'
   const data = Object.assign({}, commonParams, {
     w: query,
     p: page,
